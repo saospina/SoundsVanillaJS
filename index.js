c769b598384c6f47a796f20d5061408fb3c88ce4
saospina <sergos0617@gmail.com>
2020-06-11 18:54:41 -0500

    adding Click event on the pads

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 window.addEventListener('load', () => {
-     const SOUNDS = document.querySelectorAll('.sounds');
+     const SOUNDS = document.querySelectorAll('.sound');
      const PADS = document.querySelectorAll('.pads div');
+     console.log(SOUNDS[0]);
+
+     PADS.forEach((pad, index) => {
+         pad.addEventListener('click', () => {
+             SOUNDS[index].play();
+         })
+     })
+     
      
 })
